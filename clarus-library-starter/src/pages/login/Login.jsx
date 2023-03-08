1da20317b091741a8238986f40daa662f75ff7cf
Saliha <salihaoguz2011@gmail.com>
2023-03-08 14:38:33 +0000

    login.styled

--- clarus-library-starter/src/pages/login/Login.jsx
+++ clarus-library-starter/src/pages/login/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledInput,
+} from "./Login.style";
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <LoginContainer>
+      <FormContainer>
+        <Header>LOGIN HERE</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" required />
+          <StyledInput type="password" required />
+          <StyledButton type="submit">LOGIN</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
 
-export default Login
+export default Login;
