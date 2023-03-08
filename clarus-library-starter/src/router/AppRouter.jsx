5fc66a3f39c3eb217207cb813b011c0cd1a1c149
Saliha <salihaoguz2011@gmail.com>
2023-03-08 13:29:36 +0000

    routes

--- clarus-library-starter/src/router/AppRouter.jsx
+++ clarus-library-starter/src/router/AppRouter.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import About from "../pages/about/About";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
