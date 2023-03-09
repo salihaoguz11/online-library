92bf50d768a7468032ad37e82f396a7c6a6f17b5
Saliha <salihaoguz2011@gmail.com>
2023-03-09 14:33:10 +0000

    card style

--- clarus-library-starter/src/components/card/Card.jsx
+++ clarus-library-starter/src/components/card/Card.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
-const Card = () => {
+import React from "react";
+import { CardButton, CardContainer, CardHeader, CardImg } from "./Card.style";
+import defaultImg from "../../assets/book.jpg";
+import { useNavigate } from "react-router-dom";
+const Card = ({ item }) => {
+  const navigate = useNavigate();
   return (
-    <div>Card</div>
-  )
-}
+    <CardContainer>
+      <CardHeader>{item.volumeInfo.title}</CardHeader>
+      <CardImg src={item.volumeInfo.imageLinks?.smallThumbnail || defaultImg} />
 
-export default Card
+      <CardButton
+        onClick={() => navigate(`/detail/${item.id}`, { state: item })}
+      >
+        View More
+      </CardButton>
+    </CardContainer>
+  );
+};
+
+export default Card;
