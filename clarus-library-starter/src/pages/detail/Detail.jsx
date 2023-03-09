bd4e49b3413ac61c5dda92a6cb80bbfaddd23c3d
Saliha <salihaoguz2011@gmail.com>
2023-03-09 14:42:03 +0000

    detail-style

--- clarus-library-starter/src/pages/detail/Detail.jsx
+++ clarus-library-starter/src/pages/detail/Detail.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 const Detail = () => {
+  const { state } = useLocation();
+  const { id } = useParams();
   return (
-    <div>Detail</div>
+    
   )
-}
+};
 
-export default Detail
+export default Detail;
